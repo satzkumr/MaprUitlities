f90ee27227f69bfe288793a26ec63f6c1d687c95
satzkumr <mrsathishkumar12@gmail.com>
2017-03-22 12:47:37 +0530

    Added Comments

--- CreateTable.java
+++ CreateTable.java
@@ -18,8 +18,8 @@ public class CreateTable {
   public static void main(String[] args) throws IOException {
     Configuration configuration = HBaseConfiguration.create();
     HBaseAdmin admin = new HBaseAdmin(configuration);
-    HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("/tmp/Sathish_table_new"));	// To user different cluster use: <clustername>/<path here>
-    //Example: /mynewcluster/tmp/createtable
+    HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("/tmp/Sathish_table_new"));	// To user different cluster use: maprfs://<clustername>/<path here>
+    //Example: maprfs://mynewcluster/tmp/createtable, If you dont mention any clsuter name here, By default it uses the current cluster where you run the code
     
     tableDescriptor.addFamily(new HColumnDescriptor("cf1"));
     tableDescriptor.addFamily(new HColumnDescriptor("cf2"));
